9f93a52adedae42f0ad57293a551768a814a28a5
Hayden McKinney <hemc7@sbcglobal.net>
2024-01-30 21:56:19 +0900

    removed about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def about
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  get 'home/about'
   #root 'home#index'
   root 'friends#index'
 
