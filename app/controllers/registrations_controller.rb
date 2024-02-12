efa87383c98ecf5a50926c1ca953436760bd9391
Hayden McKinney <hemc7@sbcglobal.net>
2024-02-12 18:36:34 +0900

    added a first and last name field for each user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,18 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    super do |resource|
+      resource.update(
+        first_name: params[:user][:first_name],
+        last_name: params[:user][:last_name]
+      )
+    end
+  end
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :friends
   #root 'home#index'
   root 'friends#index'
